1b659681bb91577d7229382d789f9bfed60d0bc1
abhinav <abhinavnair2nt5@gmail.com>
2026-04-27 11:15:39 +0530

    Setup email service for future verification

---
+++ backend/src/services/email.service.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  // Create a transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: process.env.EMAIL_SECURE === 'true', // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  // Define email options
+  const message = {
+    from: process.env.EMAIL_FROM,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html: options.html // we can add HTML emails later
+  };
+
+  // Send the email
+  const info = await transporter.sendMail(message);
+  console.log('Message sent: %s', info.messageId);
+};
+
+module.exports = sendEmail;
